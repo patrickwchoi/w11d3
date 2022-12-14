e659002fb66a554186d83b06ade932515fad0a4b
Patrick Choi <ptrckchoi@gmail.com>
2022-12-14 14:04:34 -0800

    finished q1

--- practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SelectedCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SelectedCoffeeBean.js
@@ -1,5 +1,14 @@
+import { CoffeeProvider, useCoffeeTheme } from "../context/CoffeeContext";
+// import { useContext } from "react";
+
 const SelectedCoffeeBean = () => {
-  return null;
+  const { coffeeBean, setCoffeeBeanId } = useCoffeeTheme();
+
+  return (
+    <div className="selected-coffee"> 
+      <h2>{coffeeBean.name}</h2>
+    </div>
+  );
 }
 
 export default SelectedCoffeeBean;
--- practice-for-week-14-react-context-review-with-tests-long-practice/src/context/CoffeeContext.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/context/CoffeeContext.js
@@ -1,7 +1,11 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import coffeeBeans from '../mockData/coffeeBeans.json';
 
 export const CoffeeContext = createContext();
+
+export const useCoffeeTheme = () => {
+  return useContext(CoffeeContext)
+}
 
 export default function CoffeeProvider(props) {
   const [coffeeBean, setCoffeeBean] = useState(coffeeBeans[0]);
