6505f33a89dbb84a0d30f92c81a2e3cb6a992c6c
remnantsofred <remnantsofred@gmail.com>
2022-12-14 14:54:49 -0800

    Finish Context review

--- practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
@@ -1,9 +1,15 @@
+// this is us using a custom use context hook 
+import { CoffeeProvider, useCoffeeTheme } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { coffeeBean, setCoffeeBeanId } = useCoffeeTheme();
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        onChange={e => setCoffeeBeanId(e.target.value)}
+        value={coffeeBean.id}
       >
         {coffeeBeans.map(bean => (
           <option
